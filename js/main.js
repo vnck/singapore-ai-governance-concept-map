65fd92414c69e6e695b2e7ee8e8ea680711cb3f5
vnck <ivanckj96@gmail.com>
2021-07-08 23:26:41 +0800

    back button

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 $(document).ready(() => {
     $('#titleHeader').on('click', e => {
-        e.preventDefault();
+        e.preventDefault(); 
         $('html, body').animate({
             scrollTop: $("#introHeader").offset().top
           }, 800
         );
-    })
+        $("#backUpContainer").css('visibility','visible');   
+        $("#backUpContainer").fadeIn('fast');
+    });
+
+    $(window).scroll(() => { 
+        if ($(this).scrollTop() > 100) { 
+            $("#backUpContainer:hidden").css('visibility','visible');   
+            $("#backUpContainer:hidden").fadeIn('fast');  
+        } 
+        else {     
+            $("#backUpContainer:visible").fadeOut("fast"); 
+        }  
+    });
+
+    $('#backUpContainer').on('click', e => {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+          }, 800
+        );
+    });
 });
